9f71605dbbaff34b584203306a20d5b81f2c478d
gvniekerk011@gmail.com <gvniekerk011@gmail.com>
2019-09-05 19:55:53 +0200

    Added controls to Customers form

--- Customers.Designer.cs
+++ Customers.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnAddCust = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(569, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnView
+            // 
+            this.btnView.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnView.Location = new System.Drawing.Point(597, 12);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(181, 32);
+            this.btnView.TabIndex = 1;
+            this.btnView.Text = "View all";
+            this.btnView.UseVisualStyleBackColor = true;
+            // 
+            // btnAddCust
+            // 
+            this.btnAddCust.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCust.Location = new System.Drawing.Point(596, 65);
+            this.btnAddCust.Name = "btnAddCust";
+            this.btnAddCust.Size = new System.Drawing.Size(181, 32);
+            this.btnAddCust.TabIndex = 2;
+            this.btnAddCust.Text = "Add Customer";
+            this.btnAddCust.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(596, 121);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(182, 34);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove Customer";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChange.Location = new System.Drawing.Point(596, 175);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(181, 33);
+            this.btnChange.TabIndex = 4;
+            this.btnChange.Text = "Change Customer";
+            this.btnChange.UseVisualStyleBackColor = true;
+            // 
+            // Customers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAddCust);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Customers";
             this.Text = "Customers";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnAddCust;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnChange;
     }
 }
