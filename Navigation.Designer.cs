49d79cf7fb041cc4dd9d5ce5f5b8ee68a4148e5c
gvniekerk011@gmail.com <gvniekerk011@gmail.com>
2019-09-05 20:36:36 +0200

    Created navigation screen

---
+++ Navigation.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Finale_Projek_V2._0
+{
+    partial class Navigation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProducts = new System.Windows.Forms.Button();
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.btnSuppliers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.Location = new System.Drawing.Point(36, 205);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Size = new System.Drawing.Size(158, 39);
+            this.btnProducts.TabIndex = 0;
+            this.btnProducts.Text = "Manage Products";
+            this.btnProducts.UseVisualStyleBackColor = true;
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Location = new System.Drawing.Point(236, 205);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(158, 39);
+            this.btnCustomers.TabIndex = 1;
+            this.btnCustomers.Text = "Manage Customers";
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.Location = new System.Drawing.Point(418, 205);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(158, 39);
+            this.btnEmployees.TabIndex = 2;
+            this.btnEmployees.Text = "Manage Employees";
+            this.btnEmployees.UseVisualStyleBackColor = true;
+            // 
+            // btnReport
+            // 
+            this.btnReport.Location = new System.Drawing.Point(36, 274);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(158, 39);
+            this.btnReport.TabIndex = 3;
+            this.btnReport.Text = "Reporting";
+            this.btnReport.UseVisualStyleBackColor = true;
+            // 
+            // btnSuppliers
+            // 
+            this.btnSuppliers.Location = new System.Drawing.Point(236, 274);
+            this.btnSuppliers.Name = "btnSuppliers";
+            this.btnSuppliers.Size = new System.Drawing.Size(158, 39);
+            this.btnSuppliers.TabIndex = 4;
+            this.btnSuppliers.Text = "Manage Suppliers";
+            this.btnSuppliers.UseVisualStyleBackColor = true;
+            // 
+            // Navigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSuppliers);
+            this.Controls.Add(this.btnReport);
+            this.Controls.Add(this.btnEmployees);
+            this.Controls.Add(this.btnCustomers);
+            this.Controls.Add(this.btnProducts);
+            this.Name = "Navigation";
+            this.Text = "Navigation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProducts;
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Button btnEmployees;
+        private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.Button btnSuppliers;
+    }
+}
