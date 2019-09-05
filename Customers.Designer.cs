4f11d4242ee8cb1a62ead5f4e19e6cd23390e7e1
gvniekerk011@gmail.com <gvniekerk011@gmail.com>
2019-09-05 20:12:57 +0200

    button

--- Customers.Designer.cs
+++ Customers.Designer.cs
@@ -33,6 +33,7 @@
             this.btnAddCust = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
             this.btnChange = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -84,11 +85,21 @@
             this.btnChange.Text = "Change Customer";
             this.btnChange.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(679, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Customers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnChange);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.btnAddCust);
@@ -108,5 +119,6 @@
         private System.Windows.Forms.Button btnAddCust;
         private System.Windows.Forms.Button btnRemove;
         private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button button1;
     }
 }
