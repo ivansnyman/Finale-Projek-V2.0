4db22cd515cc16229bdac4e6e4e23aefc0bb49a3
baren <baren@DESKTOP-OCRF807>
2019-09-07 15:49:34 +0200

    Revert "changes"
    This reverts commit dcc65a29369ed70f96cba66f6d8d7db2d47c3161.

--- Add_Product.cs
+++ Add_Product.cs
@@ -7,29 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace Finale_Projek_V2._0
 {
     public partial class Add_Product : Form
     {
-        SqlConnection con;
-
         public Add_Product()
         {
             InitializeComponent();
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            MessageBox.Show("Conected");
-        }
-
-        private void Add_Product_Load(object sender, EventArgs e)
-        {
-            String conectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\baren\Source\Repos\ivansnyman\Finale-Projek-V2.0\Supplement_Database.mdf;Integrated Security=True";
-            con = new SqlConnection(conectionString);
         }
     }
 }
