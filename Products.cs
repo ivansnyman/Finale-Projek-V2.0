72d1bbf6fea1155c437cbf26f560356f7a96bd85
gvniekerk011@gmail.com <gvniekerk011@gmail.com>
2019-09-07 16:08:15 +0200

    Coded the Refresh Button on the product form

--- Products.cs
+++ Products.cs
@@ -14,6 +14,8 @@ namespace Finale_Projek_V2._0
 {
     public partial class Products : Form
     {
+
+        SqlConnection con;
         public Products()
         {
             InitializeComponent();
@@ -29,6 +31,31 @@ namespace Finale_Projek_V2._0
             Add_Product prod1 = new Add_Product();
             prod1.MdiParent = this;
             prod1.Show();
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Gerhard\\Source\\Repos\\ivansnyman\\Finale-Projek-V2.0\\Supplement_Database.mdf;Integrated Security=True");
+
+            con.Open();
+            String sql;
+            sql = "Select * From Products";
+            SqlCommand command;
+            SqlDataReader reader;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            command = new SqlCommand(sql, con);
+            adapter.SelectCommand = command;
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds,"Products");
+
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "Products";
+
+
+            reader = command.ExecuteReader();
+
+            con.Close();
         }
     }
 }
