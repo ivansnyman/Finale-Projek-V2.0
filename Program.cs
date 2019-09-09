5ca255712b001d56226558a10abd85aae350d0da
baren <baren@DESKTOP-OCRF807>
2019-09-09 13:36:09 +0200

    Worked on Employee form

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Finale_Projek_V2._0
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Customers());
+            Application.Run(new Sales());
         }
     }
 }
