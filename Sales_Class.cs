dc7a1c37e0cebf85ebfc5d06972d4def2eecc3b3
IVAN SNYMAN <ivan.snyman@gmail.com>
2019-10-06 18:38:48 +0200

    Fixed bugs on reporting

--- Sales_Class.cs
+++ Sales_Class.cs
@@ -8,7 +8,7 @@ namespace Finale_Projek_V2._0
 {
     class Sales_Class
     {
-        private int transaction_ID, cust_ID, emp_ID;
+        private int transaction_ID, cust_ID, emp_ID, prod_ID;
         private double amount;
         private string date;
         private string products;
@@ -45,6 +45,18 @@ namespace Finale_Projek_V2._0
             set
             {
                 this.emp_ID = value;
+            }
+        }
+
+        public int Prod_ID
+        {
+            get
+            {
+                return this.prod_ID;
+            }
+            set
+            {
+                this.prod_ID = value;
             }
         }
 
@@ -90,17 +102,19 @@ namespace Finale_Projek_V2._0
             this.transaction_ID = 0;
             this.cust_ID = 0;
             this.emp_ID = 0;
+            this.prod_ID = 0;
             this.amount = 0;
             this.date = "";
             this.products = "";
         }
 
         //Constructor
-        public Sales_Class(int transaction_ID, int cust_ID, int emp_ID, double amount, string date, string products)
+        public Sales_Class(int transaction_ID, int cust_ID, int emp_ID, int prod_ID, double amount, string date, string products)
         {
             this.transaction_ID = transaction_ID;
             this.cust_ID = cust_ID;
             this.emp_ID = emp_ID;
+            this.prod_ID = prod_ID;
             this.amount = amount;
             this.date = date;
             this.products = products;
