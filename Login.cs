bc20da21aac65bcdecf4829690becdacadf53df0
gvniekerk011@gmail.com <gvniekerk011@gmail.com>
2019-10-07 10:56:57 +0200

    Fixed login bug

--- Login.cs
+++ Login.cs
@@ -25,7 +25,7 @@ namespace Finale_Projek_V2._0
 
         private void Login_Load(object sender, EventArgs e)
         {
-            String cnn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\baren\Source\Repos\ivansnyman\Finale-Projek-V2.0\Supplement_Database.mdf;Integrated Security=True";
+            String cnn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Gerhard\Source\Repos\ivansnyman\Finale-Projek-V2.0\Supplement_Database.mdf;Integrated Security=True";
             con = new SqlConnection(cnn);
         }
 
